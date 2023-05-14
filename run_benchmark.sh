be8c6fec9a8c1fb5d791d57525bf9ea4f43aa0d7
Dan Mishin <danila.mishin.2001@gmail.com>
2023-05-14 13:08:27 +0000

    Fixed script

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,5 +1,4 @@
-cat benchmarks.txt | while read line
-do
+grep -v '^#' benchmarks.txt | while read -r file ; do
    echo "==================== TESTING: $line ===================="
    python3 clusterize.py --cluster-config-path $line
 done
